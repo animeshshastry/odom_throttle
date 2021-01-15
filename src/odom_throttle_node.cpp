c8a169dff44847e2fc6de03659415dbb448c0686
animeshshastry <animesh.shastryfb@gmail.com>
2021-01-15 17:57:45 -0500

    accel propagation

--- src/odom_throttle_node.cpp
+++ src/odom_throttle_node.cpp
@@ -2,47 +2,92 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/Vector3.h"
+#include "sensor_msgs/Imu.h"
 
 #include <sstream>
+
+double accx, accy, accz;
+double current_time, prev_time, dt;
+double qw, qx, qy, qz;
+double RTe3_1, RTe3_2, RTe3_3;
 
 // nav_msgs::Odometry odom_msg;
 geometry_msgs::Point VIO_pos;
 geometry_msgs::Vector3 VIO_vel;
 
+void acc_callback(const sensor_msgs::Imu::ConstPtr& msg)
+{
+	accx = msg->linear_acceleration.z;
+	accy = msg->linear_acceleration.x;
+	accz = msg->linear_acceleration.y;
+
+	accx -= RTe3_1*(9.81);
+	accy -= RTe3_2*(9.81);
+	accz -= RTe3_3*(9.81);
+}
+
 void odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
 {
-  // odom_msg = &msg;
-  VIO_pos = msg->pose.pose.position;
-  VIO_vel = msg->twist.twist.linear;
+	// odom_msg = &msg;
+	VIO_pos = msg->pose.pose.position;
+	VIO_vel = msg->twist.twist.linear;
+
+	qw = msg->pose.pose.orientation.w;
+	qx = msg->pose.pose.orientation.x;
+	qy = msg->pose.pose.orientation.y;
+	qz = msg->pose.pose.orientation.z;
+
+	RTe3_1 = 2.0*(qx*qz - qy*qw);
+	RTe3_2 = 2.0*(qy*qz + qw*qx);
+	RTe3_3 = 1.0 - 2.0*(qx*qx + qy*qy);
+
+}
+
+void propagate(){
+
+	current_time = ros::Time::now().toSec();
+	dt = current_time - prev_time;
+	prev_time = current_time;
+
+	VIO_pos.x += VIO_vel.x*dt;
+	VIO_pos.y += VIO_vel.y*dt;
+	VIO_pos.z += VIO_vel.z*dt;
+
+	VIO_vel.x += accx*dt;
+	VIO_vel.y += accy*dt;
+	VIO_vel.z += accz*dt;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "VIO_odom");
-  ros::NodeHandle n;
-  // ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("VIO_odom", 1000);
-  ros::Publisher pos_pub = n.advertise<geometry_msgs::Point>("VIO_pos", 0);
-  ros::Publisher vel_pub = n.advertise<geometry_msgs::Vector3>("VIO_vel", 0);
-  ros::Subscriber sub = n.subscribe("/camera/odom/sample", 0, odom_callback);
+	ros::init(argc, argv, "VIO_odom");
+	ros::NodeHandle n;
+	// ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("VIO_odom", 1000);
+	ros::Publisher pos_pub = n.advertise<geometry_msgs::Point>("VIO_pos", 1);
+	ros::Publisher vel_pub = n.advertise<geometry_msgs::Vector3>("VIO_vel", 1);
+	ros::Subscriber odom_sub = n.subscribe("/camera/odom/sample", 1, odom_callback);
+	ros::Subscriber acc_sub = n.subscribe("/camera/accel/sample", 1, acc_callback);
 
-  ros::Rate loop_rate(100);
-  // ros::AsyncSpinner spinner(2); // Use 2 threads
+	ros::Rate loop_rate(100);
+	// ros::AsyncSpinner spinner(2); // Use 2 threads
 
-  while (ros::ok())
-  {
+	while (ros::ok())
+	{
 
-    // ROS_INFO("%s", msg.data.c_str());
+		// ROS_INFO("%s", msg.data.c_str());
 
-    // odom_pub.publish(odom_msg);
+		// odom_pub.publish(odom_msg);
 
-    ros::spinOnce();
-    pos_pub.publish(VIO_pos);
-    vel_pub.publish(VIO_vel);
-	// // spinner.start();
+		ros::spinOnce();
+		propagate();
+		pos_pub.publish(VIO_pos);
+		vel_pub.publish(VIO_vel);
+		// // spinner.start();
 
-    loop_rate.sleep();
-  }
+		// ros::Duration(0.002).sleep(); // sleep
+		loop_rate.sleep();
+	}
 
 
-  return 0;
+	return 0;
 }
